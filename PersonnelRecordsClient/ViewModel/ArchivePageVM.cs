4c97994ef8b99a086213c2d31837cec9b1ad352f
Kirya <73619010+krotOv-kiryan@users.noreply.github.com>
2022-05-09 22:34:06 +1000

    Edit StaffingListVM and ArchiveVM got the filters of the search, need to restruct the views of pages

--- PersonnelRecordsClient/ViewModel/ArchivePageVM.cs
+++ PersonnelRecordsClient/ViewModel/ArchivePageVM.cs
@@ -2,17 +2,19 @@
 using PersonnelRecordsClient.MVVM;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Threading;
 
 namespace PersonnelRecordsClient.ViewModel
 {
-    class ArchivePageVM : INotifyPropertyChanged
+    public class ArchivePageVM : INotifyPropertyChanged
     {
         private ArchiveApi selectedArchive;
         public ArchiveApi SelectedArchive
@@ -35,17 +37,113 @@ namespace PersonnelRecordsClient.ViewModel
         public ArchivePageVM(Dispatcher dispatcher)
         {
             //this.dispatcher = dispatcher;
-
             Task.Run(GetArchive);
         }
-       public ArchivePageVM()
+        public ArchivePageVM()
         {
             RemoveArchiveMetod = new CustomCommand(() =>
-                {
-                    Task.Run(RemoveArchive);
-                });            
+            {
+                Task.Run(RemoveArchive);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Archives)));
+            });
+
+            StaffingsCollectionView = CollectionViewSource.GetDefaultView(Staffings);
+            StaffingsCollectionView.Filter = FilterStaffings;
+            StaffingsCollectionView.SortDescriptions.Add(new SortDescription(nameof(StaffingApi.Id), ListSortDirection.Ascending));
+
+            WorkersCollectionView = CollectionViewSource.GetDefaultView(Workers);
+            WorkersCollectionView.Filter = FilterWorkers;
+            WorkersCollectionView.SortDescriptions.Add(new SortDescription(nameof(WorkerApi.Name), ListSortDirection.Ascending));
+
+            ImpactTypesCollectionView = CollectionViewSource.GetDefaultView(ImpactTypes);
+            ImpactTypesCollectionView.Filter = FilterImpactType;
+            ImpactTypesCollectionView.SortDescriptions.Add(new SortDescription(nameof(ImpactTypeApi.Id), ListSortDirection.Ascending));
+
+        }
+        #region
+        // Staffings
+        public ObservableCollection<StaffingApi> Staffings { get; set; }
+        public ICollectionView StaffingsCollectionView { get; set; }
+
+        private string _staffingIdFilterFilter = string.Empty;
+        public string StaffingIdFilter
+        {
+            get
+            {
+                return _staffingIdFilterFilter;
+            }
+            set
+            {
+                _staffingIdFilterFilter = value;
+                OnPropertyChanged(nameof(WorkerNameFilter));
+                StaffingsCollectionView.Refresh();
+            }
+        }
+        private bool FilterStaffings(object obj)
+        {
+            if (obj is StaffingApi staffing)
+            {
+                return staffing.Id.ToString().Contains(StaffingIdFilter, StringComparison.InvariantCultureIgnoreCase);
+            }
+            return false;
+        }
+        // Workers
+        public ICollectionView WorkersCollectionView { get; set; }
+        public ObservableCollection<WorkerApi> Workers { get; set; }
+
+        private string _workerNameFilter = string.Empty;
+        public string WorkerNameFilter
+        {
+            get
+            {
+                return _workerNameFilter;
+            }
+            set
+            {
+                _workerNameFilter = value;
+                OnPropertyChanged(nameof(WorkerNameFilter));
+                WorkersCollectionView.Refresh();
+            }
+        }
+        private bool FilterWorkers(object obj)
+        {
+            if (obj is WorkerApi worker)
+            {
+                return worker.Name.Contains(WorkerNameFilter, StringComparison.InvariantCultureIgnoreCase);
+            }
+            return false;
+        }
+        // ImpactType
+        public ICollectionView ImpactTypesCollectionView { get; set; }
+        public ObservableCollection<ImpactTypeApi> ImpactTypes { get; set; }
+        private string _impactTypeFilter = string.Empty;
+        public string ImpactTypeFilter
+        {
+            get
+            {
+                return _impactTypeFilter;
+            }
+            set
+            {
+                _impactTypeFilter = value;
+                OnPropertyChanged(nameof(ImpactTypeFilter));
+                ImpactTypesCollectionView.Refresh();
+            }
         }
 
+        private bool FilterImpactType(object obj)
+        {
+            if (obj is ImpactTypeApi impactType)
+            {
+                return impactType.Id.ToString().Contains(ImpactTypeFilter, StringComparison.InvariantCultureIgnoreCase);
+            }
+            return false;
+        }
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
         async Task GetArchive()
         {
             try
@@ -53,11 +151,6 @@ namespace PersonnelRecordsClient.ViewModel
                 var result = await Api.GetListAsync<ArchiveApi[]>("Archive");
                 Archives = new List<ArchiveApi>(result);
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Archives)));
-                //dispatcher.Invoke(() => {
-                //    Workers.Clear();
-                //    foreach (var r in result)
-                //        Workers.Add(r);
-                //});
             }
             catch (Exception e)
             {
@@ -66,9 +159,13 @@ namespace PersonnelRecordsClient.ViewModel
         }
         public async Task RemoveArchive()
         {
-            await Api.DeleteAsync<ArchiveApi>(SelectedArchive, "Archive");
+            var result = await Api.DeleteAsync<ArchiveApi>(SelectedArchive, "Archive");
+            await Task.Run(RemoveArchive);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Archives)));
         }
+
         void SignalChanged([CallerMemberName] string prop = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
     }
 }
---
+++ PersonnelRecordsClient/ViewModel/StaffingListVM.cs
@@ -0,0 +1,136 @@
+﻿using ModelApi;
+using PersonnelRecordsClient.MVVM;
+using PersonnelRecordsClient.Views.Windows.Companies;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace PersonnelRecordsClient.ViewModel.WindowsVM
+{
+    public class StaffingListVM : INotifyPropertyChanged
+    {
+        public StaffingApi selectedCompany { get; set; }
+
+        private StaffingApi selectedStaffing;
+        public StaffingApi SelectedStaffing
+        {
+            get => selectedStaffing;
+            set
+            {
+                selectedStaffing = value;
+                SignalChanged();
+            }
+        }
+
+        private List<StaffingApi> staffings;
+        public List<StaffingApi> Staffings
+        {
+            get => staffings;
+            set
+            {
+                staffings = value;
+                SignalChanged();
+            }
+        }
+
+        public CustomCommand CreateStaffing { get; set; }
+        public CustomCommand EditStaffing { get; set; }
+        public CustomCommand RemoveStaffing { get; set; }
+
+
+        public StaffingListVM(Dispatcher dispatcher)
+        {
+            CreateStaffing = new CustomCommand(() =>
+            {
+                if (SelectedStaffing == null)
+                    return;
+
+                MessageBoxResult result = MessageBox.Show("Создать новый?", "Или нет",
+                MessageBoxButton.YesNoCancel,
+                MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Cancel)
+                {
+                    return;
+                }
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    Task.Run(Add);
+                }
+
+                else if (result == MessageBoxResult.No)
+                {
+                    var staffing = new StaffingApi { Position = "example", Note = "example", Salary = 1231123 };
+                    Task<int> task = Api.PostAsync<StaffingApi>(staffing, "Staffing");
+                    SelectedStaffing = staffing;
+                }
+                Task.Run(GetStaffings);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Staffings)));
+
+            });
+
+            EditStaffing = new CustomCommand(() =>
+            {
+                StaffingList staffinglist = new StaffingList(); //передать selectedStaffing
+                staffinglist.Show();
+            });
+
+            RemoveStaffing = new CustomCommand(() =>
+            {
+                Task.Run(Delete);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Staffings)));
+            });
+        }
+
+        public async Task Add()
+        {
+
+            SelectedStaffing = new StaffingApi();
+            var result = Api.PostAsync<StaffingApi>(SelectedStaffing, "Staffing");
+
+            StaffingList staffinglist = new StaffingList();
+            staffinglist.Show();
+
+            await GetStaffings();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Staffings)));
+        }
+
+
+        public async Task Save()
+        {
+            var result = await Api.PutAsync<StaffingApi>(SelectedStaffing, "Staffing");
+            await GetStaffings();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Staffings)));
+        }
+        public async Task Delete()
+        {
+            var result = await Api.DeleteAsync<StaffingApi>(SelectedStaffing, "Staffing");
+            await GetStaffings();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Staffings)));
+        }
+        async Task GetStaffings()
+        {
+            try
+            {
+                var result = await Api.GetListAsync<StaffingApi[]>("Staffing");
+                Staffings = new List<StaffingApi>(result);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Staffings)));
+                SignalChanged("Staffings");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e}");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void SignalChanged([CallerMemberName] string prop = null) =>
+             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
