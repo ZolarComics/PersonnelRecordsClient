75bf75f576cbef13fc5341056ca4f0ae3391843d
Владислав Кузнецов <80295591+OverLOLKEK@users.noreply.github.com>
2022-05-12 22:50:49 +1000

    EditWorker

--- PersonnelRecordsClient/Views/Windows/Workers/EditWorker.xaml.cs
+++ PersonnelRecordsClient/Views/Windows/Workers/EditWorker.xaml.cs
@@ -1,17 +1,5 @@
-﻿using PersonnelRecordsClient.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PersonnelRecordsClient.ViewModel.WindowsVM;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PersonnelRecordsClient.Views.Windows.Workers
 {
