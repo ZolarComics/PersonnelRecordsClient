1bb5fcdd9c4079e49bd5fb6f444432385c5d2ef2
Владислав Кузнецов <80295591+OverLOLKEK@users.noreply.github.com>
2022-05-12 21:50:58 +1000

    Окно входа(не работает)

---
+++ PersonnelRecordsClient/AuthorizationPOP/Aut.xaml.cs
@@ -0,0 +1,76 @@
+﻿using PersonnelRecordsClient.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PersonnelRecordsClient.AuthorizationPOP
+{
+    /// <summary>
+    /// Логика взаимодействия для Aut.xaml
+    /// </summary>
+    public partial class Auth : Window
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "В поле логина нужно больше 5 символов";
+                textBoxLogin.Background = Brushes.Red;
+
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "В поле пароля нужно больше 5 символов";
+                passBox.Background = Brushes.Red;
+            }
+
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+            }
+
+            User authUser = null;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                authUser = db.Users.Where(b => b.Login == login && b.Pass == pass).FirstOrDefault();
+            }
+
+            if (authUser != null)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show("Неверный логин или пароль!");
+        }
+
+        private void Button_Window_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            Authorization authorization = new Authorization();
+            authorization.Show();
+            this.Close();
+        }
+    }
+}
